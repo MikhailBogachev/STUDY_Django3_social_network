2186237ee51e40562458d195f483e58ce08a702a
Михаил Богачев <bogachevmikhailirkutsk@gmail.com>
2023-01-31 06:43:23 +0800

    Modify group_posts view

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -3,6 +3,13 @@ from django.contrib.auth import get_user_model
 # Create your models here.
 User = get_user_model()
 
+class Group(models.Model):
+    title = models.CharField(max_length=75)
+    slug = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
 
 class Post(models.Model):
     text = models.TextField()
@@ -11,4 +18,11 @@ class Post(models.Model):
         User,
         on_delete=models.CASCADE,
         related_name='posts'
+    )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='groups'
     )
--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='first_page'),
-    path('group/<slug:slug>/', views.group_posts, name='posts_list'),
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
 ]

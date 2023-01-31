ce559f0565277340db5823dc39c9112cb85c6d4d
Богачев Михаил Викторович <m.bogachev@slata.com>
2023-01-31 18:46:47 +0800

    change_1

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,7 +5,7 @@ User = get_user_model()
 
 class Group(models.Model):
     title = models.CharField(max_length=75)
-    slug = models.CharField(max_length=50)
+    slug = models.SlugField()
     description = models.TextField()
 
     def __str__(self):

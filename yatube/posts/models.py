c8c53850a76248464977c88a54ed585bd5d9cf14
Михаил Богачев <bogachevmikhailirkutsk@gmail.com>
2023-02-04 14:46:59 +0800

    review

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,28 +1,33 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-# Create your models here.
+
+
 User = get_user_model()
 
+
 class Group(models.Model):
-    title = models.CharField(max_length=75)
-    slug = models.SlugField()
+    """Модель групп авторов"""
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
     description = models.TextField()
 
     def __str__(self):
         return self.title
 
+
 class Post(models.Model):
+    """Модель публикаций"""
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         User,
-        on_delete=models.CASCADE,
+        on_delete=models.DO_NOTHING,
         related_name='posts'
     )
     group = models.ForeignKey(
         Group,
         blank=True,
         null=True,
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
         related_name='groups'
     )
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,20 +2,23 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Post, Group
 
-# Create your views here.
+
 def index(request):
-    template = 'posts/index.html'
+    """Вью для отображения главной страницы с публикациями"""
+    template: str = 'posts/index.html'
     posts = Post.objects.order_by('-pub_date')[:10]
-    context = {
+    context: dict = {
         'posts': posts,
     }
     return render(request, template, context)
 
+
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
+    """Вью для отображения страниц с постами конкретной группы"""
+    template: str = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
-    context = {
+    context: dict = {
         'group': group,
         'posts': posts,
     }
